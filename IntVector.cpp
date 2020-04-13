f958d79dcc09e8a246b1871b808bab0e4e3f102c
JCasey823 <60228652+JCasey823@users.noreply.github.com>
2020-04-12 22:36:11 -0400

    Update IntVector.cpp

--- IntVector.cpp
+++ IntVector.cpp
@@ -1,56 +1,52 @@
 #include <iostream>
-//#include "IntVector.h"
+#include "IntVector.h"
 using namespace std;
-
-class IntVector {
-private:
-  int* array;
-  int arraysize;
-  int num_elements;
-  int expansion_factor;
-
-  void expandArray(){
-    int* new_array = new int[arraysize*2];
+// TODO: Figure out how to fill arrays and why toString doesn't work with set
+// I think I figured out the above ^^
+  void IntVector::expandArray(){
+    arraysize = arraysize*2;
+    int* temp = new int[arraysize];
     for(int i = 0; i < num_elements; ++i){
-      new_array[i] = array[i];
+      temp[i] = array[i];
     }
     delete[] array;
-    array = new_array;
-    delete[] new_array;
+    array = temp;
+    //delete[] temp;
+
 
   }
 
-public:
-  IntVector(int initialsize){
+  IntVector::IntVector(int initialsize){
     this->arraysize = initialsize;
     this->num_elements = 0;
-    array = new int[initialsize];
+    this->array = new int[initialsize];
     cout<<"Constructed"<<endl;//REMOVE LATER
 
   }
 
-  ~IntVector(){
+  IntVector::~IntVector(){
     delete[] array;
     cout<<"Destroyed"<<endl;
   }
 
-
-   void add(int value){
+   void IntVector::add(int value){
      if(num_elements == arraysize){
        expandArray();
+       cout << "Array Expanded" << endl;
      }if(num_elements == 0){
        array[num_elements] = value;
        ++num_elements;
+       cout<< "Added: "<< value<< endl;
      }else{
        int last = num_elements;
        array[last] = value;
-       //cout<<"Added: "<<value<<endl;
+       cout<<"Added: "<<value<<endl;
        ++num_elements;
      }
 
    }
 
-   void removeLast(){
+   void IntVector::removeLast(){
      int last = num_elements-1;
      //int* new_array = new int[arraysize-1];
      //int last = length;
@@ -62,41 +58,49 @@ public:
     // delete[] new_array;
    }
 
-   void remove(int index){
-     int* new_array = new int[arraysize-1];
+   void IntVector::remove(int index){
+     int* new_array = new int[arraysize];
      for(int i = 0; i < index; ++i){
-       *(new_array+i) = *(array+i);
+       new_array[i] = array[i];
      }
-     for(int j = index+1; j < num_elements-1; ++j){
-        *(new_array+j) = *(array+j);
+     for(int j = index; j < num_elements; ++j){
+        new_array[j] = array[j+1];
      }
      delete[] array;
      array = new_array;
-     delete[] new_array;
+     //delete[] new_array;
      --num_elements;
    }
 
-   int get(int index) const{
+   int IntVector::get(int index) const{
      return array[index];
    }
 
-   void set(int index, int val){
+   void IntVector::set(int index, int val){
      array[index] = val;
    }
 
-   void toString() const{
-     //std::string str = "";
-     for(int i = 0; i < num_elements;++i){
-      // std::string str2 = std::to_string(array[i]);
-      //string str = str + str2;
-      cout<<*(array+i)<<endl;
-     }
-     // str;
+   void IntVector::showSize(){
+     cout<< "The size of your vector is: "<< num_elements<< " elements"<<endl;
    }
-};
+
+    std::string IntVector::toString() const{
+     std::string str = "";
+     for(int i = 0; i < num_elements;++i){
+      if(i < num_elements-1){
+        str = str + std::to_string(array[i]) + (", ");
+      }else{
+        str = str + std::to_string(array[i]);
+     }
+   }
+     str = "[" + str + "]";
+     return str;
+   }
+
+   
 
 int main(int argc, char** argv){
-  IntVector v(3);
+  IntVector v(9);
   v.add(1);
   v.add(2);
   v.add(3);
@@ -107,11 +111,16 @@ int main(int argc, char** argv){
   v.add(8);
   v.add(9);
   v.add(10);
-  v.set(0,1000);
-  v.set(1,2000);
+  v.remove(7);
+  v.remove(1);
+  v.set(7,1000);
+  v.set(8,2000);
   cout<<v.get(7)<<endl;
+  v.add(42);
   v.removeLast();
-  v.toString();
+  v.add(42);
+  //v.toStr();
+  cout<<v.toString()<<endl;
+  v.showSize();
   return 0;
 }
-
